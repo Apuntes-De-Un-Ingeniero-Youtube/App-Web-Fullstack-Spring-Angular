f680d4ae36686ebdcd4412529017ebca27447ac0
Apuntes-De-Un-Ingeniero-Youtube <juank2001estevezvargas@gmail.com>
2022-04-06 13:41:13 -0500

    First Commit
    Close #1 Close #2 Close #3

---
+++ src/main/java/com/juan/estevez/app/AppWebFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.juan.estevez.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebFullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juan/estevez/app/AppWebFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.estevez.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
